25ab30ccd111848a912a238a126546df50352e5b
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-11-20 16:31:46 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Footer from "Footer.js"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      import Footer from ""
+    </div>
+  );
+}
+
+export default App;
